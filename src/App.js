b51154c02ecf43486321ba4c875ec80c7ce389f8
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-04-15 14:28:06 +0900

    Add: TodoList

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {createGlobalStyle} from "styled-components";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
 
 // 글로벌 스타일을 추가할 땐 createGlobalStyle을 사용한다
 const GlobalStyle = createGlobalStyle`
@@ -16,6 +17,7 @@ function App() {
       <GlobalStyle />
       <TodoTemplate>
         <TodoHead />
+        <TodoList />
       </TodoTemplate>
     </>
   );
